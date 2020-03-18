08b6bd17c4132beed573a92bee95f77b10f441e9
GregVaz <grvazquezya@ittepic.edu.mx>
2020-03-18 09:57:45 -0700

    Columnas y estilos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
+
+  //Instancia de la clase Style
+  final estiloTexto = new TextStyle(fontSize: 25);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +13,13 @@ class HomePage extends StatelessWidget{
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Hola Mundo')
+        child: Column(
+          children: <Widget>[
+            Text('Numero de clicks:', style: estiloTexto),
+            Text('0', style: estiloTexto),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        )
       ),
     );
   }
